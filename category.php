3b525b4d36477b0babac566d8952e10e4d254b16
Moumy Ndiaye <e2177120@cmaisonneuve.qc.ca>
2023-10-27 17:10:08 -0400

    Ajout de code pour la gestion des sessions

--- category.php
+++ category.php
@@ -1,44 +1,44 @@
 <?php
 /**
- * category.php est le modèle par défaut pour afficher une archive d'articles de catégorie spécifique
- * afficher une archive d'articles de catégorie spécifique
+ * category.php est le modèle par défaut pour afficher une archive d'articles de catégorie spécifique.
  */
-?>
 
-<?php get_header(); ?>
+get_header();
+?>
 
 <main class="site__main">
     <section class="section__categorie">
         <?php
-            $category = get_queried_object();
+        $category = get_queried_object();
         ?>
-        <!-- Affiche les titre et nom de catégorie -->
         <div class="conteneur__texte">
             <h1><?php single_cat_title(); ?></h1>
             <p><?php the_archive_description(); ?></p>
         </div>
-        <!-- Affiche le menu a option -->
+
+        <!-- Affiche le menu à option -->
         <div class="conteneur__option">
             <?php
-                // Vérifiez si la catégorie a un slug égal à "cours"
-                if ($category->slug === 'cours') {
-                    ?>
-                    <select name="sessions" id="sessions">
-                        <option value="1" selected>Session 1</option>
-                        <option value="2">Session 2</option>
-                        <option value="3">Session 3</option>
-                        <option value="4">Session 4</option>
-                        <option value="5">Session 5</option>
-                        <option value="6">Session 6</option>
-                    </select>
-                    <?php
-                }
+            // Vérifiez si la catégorie a un slug égal à "cours"
+            if ($category->slug === 'cours') {
+                ?>
+                <select name="sessions" id="sessions">
+                    <option value="1" selected>Session 1</option>
+                    <option value="2">Session 2</option>
+                    <option value="3">Session 3</option>
+                    <option value="4">Session 4</option>
+                    <option value="5">Session 5</option>
+                    <option value="6">Session 6</option>
+                </select>
+                <?php
+            }
             ?>
         </div>
+
         <!-- Affiche les articles -->
-        <div class="conteneur__bloc" id="articles-container">
+        <div class="conteneur__bloc" id="articles-containeur">
             <?php
-            // Affichez les articles de la catégorie initiale
+            // Affichez les articles de la catégorie initiale (cours)
             $args = array(
                 'category_name' => $category->slug,
                 'orderby' => 'title',
@@ -46,21 +46,23 @@
             );
             $query = new WP_Query($args);
 
-            if ($query->have_posts()) :
+            if ($query->have_posts() && $category->slug != 'cours') :
                 while ($query->have_posts()) : $query->the_post();
                     get_template_part('template-parts/categorie', $category->slug);
-                    // Affichez le contenu de chaque article ici
                 endwhile;
                 wp_reset_postdata();
             endif;
             ?>
         </div>
-        <!-- Affiche les menus secondaire -->
-        <div class="conteneur__menu">                
-            <?php 
+
+        <!-- Affiche les menus secondaires -->
+        <div class="conteneur__menu">
+            <?php
             // Correspondance entre les catégories et les noms de menu
             $menu_correspondance = array(
                 'etudiants' => 'menu-etudiants',
+                'cours' => 'menu-etudiants',
+                'profs' => 'menu-etudiants',
                 'projets' => 'menu-projets'
                 // Ajoutez d'autres correspondances au besoin
             );
@@ -80,4 +82,32 @@
     </section>
 </main>
 
-<?php get_footer(); ?>
+<?php get_footer();
+?>
+
+<script>
+    function loadSessionArticles(selectedSession) {
+        var articlesContaineur = document.getElementById('articles-containeur');
+        articlesContaineur.innerHTML = '<span class="texte__chargement">Chargement des articles de la session ' + selectedSession + '...</span>';
+
+        // Chargez dynamiquement les articles de session en fonction de l'option sélectionnée
+        fetch('<?php echo admin_url('admin-ajax.php'); ?>?action=load_session_articles&selectedSession=' + selectedSession)
+            .then(response => response.text())
+            .then(data => {
+                articlesContaineur.innerHTML = data;
+            });
+    }
+
+    (function () {
+        var sessionsSelect = document.getElementById('sessions');
+        if (sessionsSelect) {
+            // Gérer le changement de session lors du chargement de la page
+            loadSessionArticles(sessionsSelect.value);
+
+            // Ajouter un gestionnaire d'événements pour le changement de session
+            sessionsSelect.addEventListener('change', function () {
+                loadSessionArticles(this.value);
+            });
+        }
+    })();
+</script>
--- single.php
+++ single.php
@@ -7,6 +7,17 @@
     <?php get_header(); ?>    
 
         <main class="site__main">
+            <?php if(have_posts()): 
+                while(have_posts()): the_post(); ?>
+                    <article class="dans__article"> 
+                        <div>
+                            <?php the_post_thumbnail('thumbnail'); ?>
+                            <h3><?php the_title(); ?></h3>
+                        </div>
+                        <?php the_content(); ?>
+                    </article>
+                <?php endwhile; ?>
+            <?php endif; ?>
         </main>
 
     <?php get_footer(); ?>   

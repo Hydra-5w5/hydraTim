a23a1e278e8dc3a74fabd1263168767beb7a3b6c
Kayembe <e2052331@cmaisonneuve.qc.ca>
2023-11-13 13:36:05 -0500

    Programmation finale de toutes zones de recherche

--- 404.php
+++ 404.php
@@ -7,6 +7,7 @@
         <main class="site__erreur">
             <h1>Erreur 404</h1>
             <h3>Page introuvable, vous pouvez tenter une recherche</h3>
+            <?php  get_search_form(); ?> 
         </main>
     <?php get_footer(); ?>   
 </body>

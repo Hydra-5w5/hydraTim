6b717502d8a732ea971b647bb5e67ddb1fbf0a35
Naz <nazeefsuleman@gmail.com>
2023-11-14 22:08:46 -0500

    changements mineures

--- category.php
+++ category.php
@@ -57,7 +57,7 @@ get_header();
                 endif;
 
                 // Vérifie si la catégorie actuelle est "prof" ou "cours" pour afficher les boutons
-                if ($category->slug === 'profs' || $category->slug === 'temoignage') {
+                if ($category->slug === 'profs' || $category->slug === 'cours' ||$category->slug === 'temoignage') {
                     echo '<div class="btn__gauche">Précédent</div>';
                     echo '<div class="btn__droite">Suivant</div>';
                 }

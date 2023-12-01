b1b4ba4ac6832d0c8fc0596e08b7423fbc1cbd44
Kayembe <e2052331@cmaisonneuve.qc.ca>
2023-12-01 11:51:23 -0500

    Finalisation de toutes les barres des recherches.Page erreur 404, zone recherche, zone recherche footer

--- 404.php
+++ 404.php
@@ -7,9 +7,9 @@
     <?php get_header(); ?>    
     
         <main class="site__erreur">
+            <h1>Erreur 404 !</h1>
+            <h6>Page introuvable, vous pouvez tenter une recherche</h6>
             <?php get_template_part('vagues-variantes/vaguesGeneral')?> 
-            <h1>Erreur 404</h1>
-            <h3>Page introuvable, vous pouvez tenter une recherche</h3>
             <?php  get_search_form(); ?> 
         </main>
     <?php get_footer(); ?>   

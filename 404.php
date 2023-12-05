949751d58c30970e486f210b9634b3a650fa70da
Moumy Ndiaye <112096535+NMoumy@users.noreply.github.com>
2023-12-05 09:03:48 -0500

    arrangement de la page de recherche et d'erreur 404

--- 404.php
+++ 404.php
@@ -5,13 +5,16 @@
 ?>
 
     <?php get_header(); ?>    
-    
         <main class="site__erreur">
             <?php get_template_part('vagues-variantes/vaguesGeneral')?> 
-            <h1>Erreur 404</h1>
-            <h3>Page introuvable, vous pouvez tenter une recherche</h3>
-            <?php  get_search_form(); ?> 
+            <?php get_template_part('vagues-variantes/vaguesHautPiedPage'); ?>
+            <div class="info__erreur">
+                <h1>Erreur 404 !</h1>
+                <h6>Page introuvable, vous pouvez tenter une recherche</h6>
+                <?php  get_search_form(); ?> 
+            </div>
         </main>
+
     <?php get_footer(); ?>   
 </body>
 </html>

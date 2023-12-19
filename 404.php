197297d1d027e53d9d5ce8badda921f57e6c5184
Moumy Ndiaye <e2177120@cmaisonneuve.qc.ca>
2023-12-18 23:28:41 -0500

    Ajout du gift de la page 404

--- 404.php
+++ 404.php
@@ -9,6 +9,9 @@
             <?php get_template_part('vagues-variantes/vaguesGeneral')?> 
             <?php get_template_part('vagues-variantes/vaguesHautPiedPage'); ?>
             <div class="info__erreur">
+                <div class="img__anim">
+                    <img src="http://cidweb37.sg-host.com/wp-content/uploads/2023/12/CooperHahaha-1.gif" alt="">
+                </div>
                 <h1>Erreur 404 !</h1>
                 <h6>Page introuvable, vous pouvez tenter une recherche</h6>
                 <?php  get_search_form(); ?> 
